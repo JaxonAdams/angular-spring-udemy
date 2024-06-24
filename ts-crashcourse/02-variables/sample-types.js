648ee7d14169ef0304234c37c601a7e8fbf63bb3
Jaxon Adams <jaxon.adams@loanpro.io>
2024-06-24 12:49:28 -0600

    initial files

---
+++ ts-crashcourse/02-variables/sample-types.js
@@ -0,0 +1,9 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Jaxon";
+var lastName = "Adams";
+// breaking the compiler:
+// found = "true";
+console.log(found);
+console.log("Grade: ".concat(grade));
+console.log("".concat(firstName, " ").concat(lastName));
---
+++ ts-crashcourse/02-variables/sample-types.ts
@@ -0,0 +1,11 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = "Jaxon";
+let lastName: string = "Adams";
+
+// breaking the compiler:
+// found = "true";
+
+console.log(found);
+console.log(`Grade: ${grade}`);
+console.log(`${firstName} ${lastName}`);
